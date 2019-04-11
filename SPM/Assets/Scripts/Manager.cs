bff7915fb8965cb8e34b227b284b49e10936bdc0
Paffyy <pw@teamwahlin.se>
2019-04-11 16:20:23 +0200

    Sword Implementation

--- SPM/Assets/Scripts/Manager.cs
+++ SPM/Assets/Scripts/Manager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Manager
@@ -19,4 +20,23 @@ public class Manager
         }
     }
 
+    public List<Collider> GetFrontConeHit(Vector3 facingDirection, Transform transform, LayerMask layerMask, float radius, float angle)
+    {
+        var colliders = Physics.OverlapSphere(transform.position, radius, layerMask).ToList() ?? new List<Collider>();
+        for (int i = colliders.Count - 1; i >= 0; i--)
+        {
+            var direction = (colliders[i].transform.position - transform.position).normalized;
+            if (!(Vector3.Angle(facingDirection, direction) < angle / 2))
+            {
+                // TODO can hit through walls
+                colliders.Remove(colliders[i]);
+            }
+        }
+        return colliders;
+    }
+    public List<Collider> GetAoeHit(Vector3 pos, LayerMask layerMask, float radius)
+    {
+        var colliders = Physics.OverlapSphere(pos, radius, layerMask).ToList() ?? new List<Collider>();
+        return colliders;
+    }
 }
