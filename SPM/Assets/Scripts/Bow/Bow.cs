16ef7a1ff5ecd45342321f1f02aa7ddd39f6a59a
Paffyy <pw@teamwahlin.se>
2019-04-11 23:33:38 +0200

    Angle på bågen

--- SPM/Assets/Scripts/Bow/Bow.cs
+++ SPM/Assets/Scripts/Bow/Bow.cs
@@ -6,11 +6,15 @@ public class Bow : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject Arrow;
-    public GameObject parent;
+    public GameObject Parent;
+    public GameObject Player;
     public Camera playerCamera;
-    private float speed = 5;
+    private Vector3 bowOffset;
+    private float speed = 7;
+    private float angle = 0.1f;
     void Awake()
     {
+        bowOffset = new Vector3(0.5f, 0, 0f);
     }
     void Start()
     {
@@ -21,19 +25,38 @@ public class Bow : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Mouse1))
         {
-            if (speed < 45)
+            if (speed < 30)
             {
                 speed += speed * 2f * Time.deltaTime;
             }
+            if (angle < 0.3f)
+            {
+                angle += angle * Time.deltaTime;
+            }
+            Debug.Log(angle + ":" + speed);
         }
         if (Input.GetKeyUp(KeyCode.Mouse1))
         {
             var direction = playerCamera.transform.forward;
             direction = Vector3.ProjectOnPlane(direction, Vector3.down);
-            var arrow = Instantiate(Arrow, transform.position + new Vector3(0, 0, 0) + direction, Quaternion.LookRotation(direction * speed), parent.transform);
-            arrow.GetComponent<Arrow>().ApplyInitialVelocity(direction.normalized * speed);
+            var arrow = Instantiate(Arrow, transform.position + direction, Quaternion.LookRotation(direction * speed), Parent.transform);
+            arrow.GetComponent<Arrow>().ApplyInitialVelocity(direction.normalized * speed + new Vector3(0, speed / 90,0) * speed);
+           
             speed = 5;
+            angle = 0.1f;
         }
+        UpdatePosition();
+        UpdateRotation();
+    }
+    private void UpdateRotation(float swing = 0)
+    {
+        var direction = playerCamera.transform.forward;
+        transform.rotation = Quaternion.LookRotation(direction);
     }
 
+    private void UpdatePosition()
+    {
+        Vector3 update = transform.rotation * bowOffset.normalized;
+        transform.position = update * bowOffset.magnitude + Player.transform.position;
+    }
 }
