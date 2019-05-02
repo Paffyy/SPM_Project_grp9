3f89608402c68bffee5c4d9cd691e65959ce555b
taey1 <37865938+taey1@users.noreply.github.com>
2019-05-02 18:16:19 +0200

    PortalSystem PoC1

---
+++ SPM/Assets/Scripts/Portal/PortalTeleporter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTeleporter : MonoBehaviour
+{
+    public Transform Player;
+    public Transform Receiver;
+    private bool playerIsOverlapping = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerIsOverlapping)
+        {
+            Vector3 portalToPlayer = Player.position - transform.position;
+            float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
+            if(dotProduct < 0f)
+            {
+                float rotationDiff = -Quaternion.Angle(transform.rotation, Receiver.rotation);
+                rotationDiff += 180;
+                Player.Rotate(Vector3.up, rotationDiff);
+                Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
+                Player.position = new Vector3(Receiver.position.x, Receiver.position.y + 0.1f, Receiver.position.z) + positionOffset;
+                playerIsOverlapping = false;
+            }
+        }   
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerIsOverlapping = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerIsOverlapping = false;
+        }
+    }
+}
---
+++ SPM/Assets/Scripts/Portal/PortalTextureSetup.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTextureSetup : MonoBehaviour
+{
+    public Camera CameraA;
+    public Camera CameraB;
+    public Material CameraMatA;
+    public Material CameraMatB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(CameraB.targetTexture != null)
+        {
+            CameraB.targetTexture.Release();
+        }
+        CameraB.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        CameraMatB.mainTexture = CameraB.targetTexture;
+        if (CameraA.targetTexture != null)
+        {
+            CameraA.targetTexture.Release();
+        }
+        CameraA.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        CameraMatA.mainTexture = CameraA.targetTexture;
+    }
+}
