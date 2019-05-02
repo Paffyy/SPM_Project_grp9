533cdf0acac7eddfae2773e05cb1edb871ce4dab
Paffyy <pw@teamwahlin.se>
2019-05-02 17:46:20 +0200

    Revitalize zone

---
+++ SPM/Assets/Scripts/Revitalize/RevitalizeZone.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RevitalizeZone : MonoBehaviour
+{
+    public List<Enemy> Enemies;
+    public bool AllEnemiesDead;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (AllEnemiesDead)
+        {
+            RevitalizeTheZone();
+        }
+    }
+    void RevitalizeTheZone()
+    {
+        foreach (Transform child in transform)
+        {
+            var rev = child.GetComponent<RevitalizeGeometry>();
+            if (rev != null)
+            {
+                rev.Revitalize(1);
+            }
+            else
+            {
+                foreach (Transform grandchildren in child)
+                {
+                    var rev2 = grandchildren.GetComponent<RevitalizeGeometry>();
+                    if (rev2 != null)
+                    {
+                        rev2.Revitalize(1);
+                    }
+                }
+            }
+        }
+    }
+}
