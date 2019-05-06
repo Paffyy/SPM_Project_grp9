3b6b5cb910ddd26ac69d8e2f11e65f8c026d2573
Niclas Älmeby <niclas.almeby@hotmail.com>
2019-05-06 15:31:25 +0200

    events

---
+++ SPM/Assets/Scripts/EventSystem/Events/BossFightEvent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointEventInfo : BaseEventInfo
+{
+
+    public CheckPointEventInfo(GameObject newCheckPoint)
+    {
+        
+    }
+}
