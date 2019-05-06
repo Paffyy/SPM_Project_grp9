0c9425dfcfd13df8ba0950c0f63b0ce02afd970b
taey1 <37865938+taey1@users.noreply.github.com>
2019-05-06 19:18:02 +0200

    Prefabs

--- SPM/Assets/Scripts/Base Enemy/States/BaseEnemyAttackState.cs
+++ SPM/Assets/Scripts/Base Enemy/States/BaseEnemyAttackState.cs
@@ -32,9 +32,7 @@ public class BaseEnemyAttackState : BaseEnemyBaseState
 
     public override void HandleUpdate()
     {
-        Debug.Log("back off " + backOff);
-        Debug.Log("cicle player " + circle);
-        Debug.Log("to player " + toPlayer);
+
         //if(Vector3.Distance(owner.transform.position, owner.player.transform.position) < PlacmentDistance)
         //{
         //    owner.NavAgent.isStopped = true;
@@ -66,7 +64,10 @@ public class BaseEnemyAttackState : BaseEnemyBaseState
         }
         if (toPlayer)
         {
-            owner.UpdateDestination(owner.player.transform.position, 0.1f);
+            if(Vector3.Distance(owner.transform.position, owner.player.transform.position) > 1f)
+            {
+                owner.UpdateDestination(owner.player.transform.position, 0.1f);
+            }
         }
         //if (circle && timer < 0)
         //{
@@ -135,12 +136,15 @@ public class BaseEnemyAttackState : BaseEnemyBaseState
 
         //Skadar spelarn
         GameObject[] arr = owner.Fow.TargetsInFieldOfView();
+        if(arr != null)
+        {
             for (int i = 0; i < arr.Length; i++)
             {
                 Debug.Log(arr[i]);
                 PlayerHealth player = arr[i].GetComponent<PlayerHealth>();
-                Vector3 push = (((player.transform.position) - owner.transform.position).normalized + Vector3.up * 5) * 10;
+                Vector3 push = (((player.transform.position) - owner.transform.position).normalized + Vector3.up * 2) * 4;
                 player.TakeDamage(owner.Damage, push);
+            }
         }
         //arr[0].GetComponent<Player>.Hit();
         currentCooldown = cooldown;
