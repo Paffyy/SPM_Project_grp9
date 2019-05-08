b352a72d66c97c683c6217838263712effe4fea6
taey1 <37865938+taey1@users.noreply.github.com>
2019-05-09 01:16:33 +0200

    W1 Prep

--- SPM/Assets/Scripts/Portal/PortalTeleporter.cs
+++ SPM/Assets/Scripts/Portal/PortalTeleporter.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PortalTeleporter : MonoBehaviour
 {
@@ -13,17 +14,18 @@ public class PortalTeleporter : MonoBehaviour
     {
         if (playerIsOverlapping)
         {
-            Vector3 portalToPlayer = Player.position - transform.position;
-            float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
-            if(dotProduct < 0f)
-            {
-                float rotationDiff = -Quaternion.Angle(transform.rotation, Receiver.rotation);
-                rotationDiff += 180;
-                Player.Rotate(Vector3.up, rotationDiff);
-                Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
-                Player.position = new Vector3(Receiver.position.x, Receiver.position.y + 0.1f, Receiver.position.z) + positionOffset;
-                playerIsOverlapping = false;
-            }
+            SceneManager.LoadScene(2);
+            //Vector3 portalToPlayer = Player.position - transform.position;
+            //float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
+            //if(dotProduct < 0f)
+            //{
+            //    float rotationDiff = -Quaternion.Angle(transform.rotation, Receiver.rotation);
+            //    rotationDiff += 180;
+            //    Player.Rotate(Vector3.up, rotationDiff);
+            //    Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
+            //    Player.position = new Vector3(Receiver.position.x, Receiver.position.y + 0.1f, Receiver.position.z) + positionOffset;
+            //    playerIsOverlapping = false;
+            //}
         }   
     }
 
