219a5f8b930f48ca3269f9e28d5c261006f2874a
taey1 <37865938+taey1@users.noreply.github.com>
2019-05-15 01:02:50 +0200

    Test Sword Animation PoC1

---
+++ SPM/Assets/SwordAttackAnim.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordAttackAnim : MonoBehaviour
+{
+    public Animator Anim;
+    public ParticleSystem Trails;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J)){
+            Anim.SetBool("Attack", true);
+        }
+    }
+
+    public void GoToIdle()
+    {
+        Anim.SetBool("Attack", false);
+    }
+
+    public void SpawnTrail()
+    {
+        Trails.Play();
+        Debug.Log("Start trails");
+    }
+
+    public void StopTrail()
+    {
+        Trails.Stop();
+        Debug.Log("Stop trails");
+    }
+}
