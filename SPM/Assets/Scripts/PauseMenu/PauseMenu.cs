77509b702c803bcdf7a60e01db433abf219a0ddc
Paffyy <pw@teamwahlin.se>
2019-05-19 22:39:50 +0200

    PauseMenuProto

---
+++ SPM/Assets/Scripts/PauseMenu/PauseMenu.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+   
+    public static bool IsPaused;
+    [SerializeField]
+    private GameObject pauseMenu;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (IsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    private void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        IsPaused = false;
+    }
+
+    private void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        IsPaused = true;
+    }
+}
