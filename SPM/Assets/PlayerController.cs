9b82572c1875cf36856abe2b4587d0f5e7e27316
Niclas Älmeby <niclas.almeby@hotmail.com>
2019-05-23 16:44:55 +0200

    boop

--- SPM/Assets/PlayerController.cs
+++ SPM/Assets/PlayerController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerTest : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     private PhysicsController controller;
     private CameraController cameraCon;
