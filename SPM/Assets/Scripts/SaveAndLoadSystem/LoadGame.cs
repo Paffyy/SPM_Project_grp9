0b496a1e7f1fbdcb57b2743de84d00da3abfe3b4
taey1 <37865938+taey1@users.noreply.github.com>
2019-05-23 19:54:41 +0200

    Wb1 fix

--- SPM/Assets/Scripts/SaveAndLoadSystem/LoadGame.cs
+++ SPM/Assets/Scripts/SaveAndLoadSystem/LoadGame.cs
@@ -8,6 +8,7 @@ public class LoadGame : MonoBehaviour
 
     void Start()
     {
+        Debug.Log(Manager.Instance.HasLoadedFromCheckPoint);
         if (Manager.Instance.HasLoadedFromCheckPoint)
         {
             LoadFromSave();
@@ -33,6 +34,7 @@ public class LoadGame : MonoBehaviour
         position.x = data.PlayerPosition[0];
         position.y = data.PlayerPosition[1];
         position.z = data.PlayerPosition[2];
+        Debug.Log(position);
         Player.transform.position = position;
         Player.GetComponent<PlayerHealth>().CurrentHealth = data.PlayerHealth;
         Player.GetComponent<Weapon>().ArrowCount = data.ArrowCount;
