2965eef04c38c3a4cc1fedaaaa9cc95d3ca3a573
Niclas Älmeby <niclas.almeby@hotmail.com>
2019-05-26 18:11:55 +0200

    stuff

---
+++ SPM/Assets/MainMenu/Scripts/MainMenu.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private GameObject loadingScreen;
+    void Start()
+    {
+        Register();
+    }
+
+    void Register()
+    {
+        Debug.Log(EventHandler.Instance);
+        EventHandler.Instance.Register(EventHandler.EventType.UIButtonEvent, CheckButtonClick);
+    }
+
+    private void CheckButtonClick(BaseEventInfo e)
+    {
+        var buttonEventInfo = e as UIButtonEventInfo;
+
+        if (buttonEventInfo != null)
+        {
+            switch ((int)buttonEventInfo.ButtonEnum)
+            {
+                case 0:
+                    //Resume borde vara här sen när den implementeras
+                    //Resume();
+                    NewGame();
+                    break;
+                case 1:
+                    OptionsButton();
+                    break;
+                case 2:
+                    Quit();
+                    break;
+                case 3:
+                    NewGame();
+                    break;
+            }
+
+        }
+    }
+
+
+    private void NewGame()
+    {
+        loadingScreen.SetActive(true);
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    private void Resume() { 
+
+    }  
+
+    private void OptionsButton()
+    {
+
+    }
+
+    private void Quit()
+    {
+
+    }
+
+}
