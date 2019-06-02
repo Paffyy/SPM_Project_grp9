295941a456476ac5aa415473268e3fbbde6e7c2a
taey1 <37865938+taey1@users.noreply.github.com>
2019-06-02 02:41:55 +0200

    Intro scene test stuff

---
+++ SPM/Assets/IntroScene/IntroScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroScene : MonoBehaviour
+{
+
+    void Start()
+    {
+        foreach(GameObject zone in GameController.GameControllerInstance.Zones.Values)
+        {
+            zone.GetComponent<RevitalizeZone>().RevitalizeTheZoneInstant();
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ SPM/Assets/IntroScene/Meteor/Scripts/MeteorMovement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private GameObject impactPrefab;
+    [SerializeField]
+    private List<GameObject> trails;
+    private Rigidbody rigidBody;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if (speed != 0 && rigidBody != null)
+        {
+            rigidBody.position += transform.forward * (speed * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        speed = 0;
+        if (!collision.gameObject.CompareTag("MeteorGround"))
+        {
+            Physics.IgnoreCollision(collision.collider, GetComponent<BoxCollider>());
+        }
+        else
+        {
+            ContactPoint contact = collision.contacts[0];
+            Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+            Vector3 pos = contact.point;
+            if (impactPrefab != null)
+            {
+                GameObject impactVFX = Instantiate(impactPrefab, pos, rot) as GameObject;
+                Destroy(impactVFX, 20f);
+            }
+
+            if(trails.Count > 0)
+            {
+                foreach(GameObject trail in trails)
+                {
+                    trail.transform.parent = null;
+                    ParticleSystem ps = trail.GetComponent<ParticleSystem>();
+                    if(ps != null)
+                    {
+                        ps.Stop();
+                        Destroy(ps.gameObject, ps.main.duration + ps.main.startLifetime.constantMax);
+                    }
+                }
+            }
+            Destroy(gameObject);
+        }
+    }
+}
