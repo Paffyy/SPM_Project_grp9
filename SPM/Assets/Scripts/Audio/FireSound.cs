a4b763a191f24dc4c4d7f6e4f0f3ddabe31ecb19
tovajaderberg <tova.t.jaderberg@gmail.com>
2019-06-04 15:34:27 +0200

    fire sound

---
+++ SPM/Assets/Scripts/Audio/FireSound.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireSound : MonoBehaviour
+{
+
+    private AudioSource source;
+    public AudioClip clip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        source.clip = clip;
+        source.loop = true;
+        source.time = Random.Range(0, clip.length);
+        source.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
